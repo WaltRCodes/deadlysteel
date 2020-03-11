34ab37c267659c390070d00b94d3e87df83faeee
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-11 14:44:23 -0400

    added the name parameter in the constructor

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //creating a robot class in order to encompas all the functions and values that the players will utilize
 class robot{
-    constructor(){
-        
+    constructor(name){
+        this.name = name;
     }
 }
